995df4d61692d149412e38eb9c10aca875986f99
Aoi MOTOYA <aoi.hashizume@gmail.com>
2023-07-22 20:20:31 +0900

    create GCS bucket for static web site

--- web.tf
+++ web.tf
@@ -7,3 +7,32 @@ terraform {
     }
   }
 }
+
+provider "google" {
+  project = "aoimotoya-websites"
+  region  = "asia-northeast1"
+}
+
+resource "random_id" "bucket_suffix" {
+  byte_length = 8
+}
+
+resource "google_storage_bucket" "top" {
+  name          = "web-top-${random_id.bucket_suffix.hex}"
+  location      = "asia-northeast1"
+  storage_class = "STANDARD"
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+}
+
+resource "google_storage_bucket_access_control" "public_rule" {
+  bucket = google_storage_bucket.top.id
+  role   = "READER"
+  entity = "allUsers"
+}

6d6ac3067c7ca849135665b82fc81edea19f379f
Aoi MOTOYA <aoi.hashizume@gmail.com>
2023-07-23 20:22:17 +0900

    remove http

--- web/load_balancer.tf
+++ web/load_balancer.tf
@@ -26,24 +26,6 @@ resource "google_compute_url_map" "blog" {
   default_service = google_compute_backend_bucket.blog.id
 }
 
-resource "google_compute_url_map" "redirect" {
-  name = "top-url-map-redirect-http-to-https"
-  default_url_redirect {
-    https_redirect = true
-    strip_query    = false
-  }
-}
-
-resource "google_compute_target_http_proxy" "top" {
-  name    = "top-http-proxy"
-  url_map = google_compute_url_map.redirect.id
-}
-
-resource "google_compute_target_http_proxy" "blog" {
-  name    = "blog-http-proxy"
-  url_map = google_compute_url_map.redirect.id
-}
-
 resource "google_compute_target_https_proxy" "top" {
   name    = "top-https-proxy"
   url_map = google_compute_url_map.top.id
@@ -66,15 +48,6 @@ resource "google_compute_target_https_proxy" "blog" {
   ]
 }
 
-resource "google_compute_global_forwarding_rule" "top_http" {
-  name                  = "top-http-lb-forwarding-rule"
-  ip_protocol           = "TCP"
-  load_balancing_scheme = "EXTERNAL_MANAGED"
-  port_range            = "80"
-  target                = google_compute_target_http_proxy.top.id
-  ip_address            = google_compute_global_address.top.id
-}
-
 resource "google_compute_global_forwarding_rule" "top_https" {
   name                  = "top-https-lb-forwarding-rule"
   ip_protocol           = "TCP"
@@ -82,15 +55,6 @@ resource "google_compute_global_forwarding_rule" "top_https" {
   port_range            = "443"
   target                = google_compute_target_https_proxy.top.id
   ip_address            = google_compute_global_address.top.id
-}
-
-resource "google_compute_global_forwarding_rule" "blog_http" {
-  name                  = "blog-http-lb-forwarding-rule"
-  ip_protocol           = "TCP"
-  load_balancing_scheme = "EXTERNAL_MANAGED"
-  port_range            = "80"
-  target                = google_compute_target_http_proxy.blog.id
-  ip_address            = google_compute_global_address.blog.id
 }
 
 resource "google_compute_global_forwarding_rule" "blog_https" {

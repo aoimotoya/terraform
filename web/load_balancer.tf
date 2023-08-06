f4daff20c363b2505ec396e0a8473c2ba3504b57
Aoi MOTOYA <aoi.hashizume@gmail.com>
2023-08-06 14:51:17 +0900

    add http-https redirection

--- web/load_balancer.tf
+++ web/load_balancer.tf
@@ -27,6 +27,19 @@ resource "google_compute_url_map" "default" {
   }
 }
 
+resource "google_compute_url_map" "redirect" {
+  name = "web-url-map-redirect-http-to-https"
+  default_url_redirect {
+    https_redirect = true
+    strip_query    = false
+  }
+}
+
+resource "google_compute_target_http_proxy" "default" {
+  name    = "http-proxy"
+  url_map = google_compute_url_map.redirect.id
+}
+
 resource "google_compute_target_https_proxy" "default" {
   name    = "https-proxy"
   url_map = google_compute_url_map.default.id
@@ -38,6 +51,15 @@ resource "google_compute_target_https_proxy" "default" {
     google_compute_managed_ssl_certificate.top,
     google_compute_managed_ssl_certificate.blog
   ]
+}
+
+resource "google_compute_global_forwarding_rule" "redirect" {
+  name                  = "http-lb-forwarding-rule"
+  ip_protocol           = "TCP"
+  load_balancing_scheme = "EXTERNAL_MANAGED"
+  port_range            = "80"
+  target                = google_compute_target_http_proxy.default.id
+  ip_address            = google_compute_global_address.default.id
 }
 
 resource "google_compute_global_forwarding_rule" "default" {
